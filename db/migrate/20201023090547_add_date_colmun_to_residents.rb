3bbaaee8f4701bb922ab2ab2e6edc8f8e1df4fa8
tomokitakenaga <tomoki0902@gmail.com>
2020-10-23 20:23:55 +0900

    dateのカラムのオプションの変更

--- db/migrate/20201023090547_add_date_colmun_to_residents.rb
+++ db/migrate/20201023090547_add_date_colmun_to_residents.rb
@@ -1,6 +1,6 @@
 class AddDateColmunToResidents < ActiveRecord::Migration[5.2]
   def change
-    add_column :residents, :rent_date, :date, null: false, default: 0
-    add_column :residents, :move_date, :date, null: false, default: 0
+    add_column :residents, :rent_date, :date, null: false 
+    add_column :residents, :move_date, :date, null: false
   end
 end
